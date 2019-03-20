a4a5c8d351929db316e5a0cbbb6452b0b8e0a028
Mark Forster <foz1284@gmail.com>
2019-03-20 09:21:41 +0000

    Initial Project

---
+++ EntityFrameworkInMemoryInheritanceTest/EntityFrameworkInMemoryInheritanceTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkInMemoryInheritanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
