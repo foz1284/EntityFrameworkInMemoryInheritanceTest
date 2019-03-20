518f87a191efd5be2532cf0e2aae3e0c53bef39c
Mark Forster <foz1284@gmail.com>
2019-03-20 09:52:49 +0000

    implemented basic functionality

--- EntityFrameworkInMemoryInheritanceTest/EntityFrameworkInMemoryInheritanceTest/Program.cs
+++ EntityFrameworkInMemoryInheritanceTest/EntityFrameworkInMemoryInheritanceTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EntityFrameworkInMemoryInheritanceTest
 {
@@ -6,7 +7,51 @@ namespace EntityFrameworkInMemoryInheritanceTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var databaseRoot = new InMemoryDatabaseRoot();
+            var options = new DbContextOptionsBuilder()
+                                .UseInMemoryDatabase(databaseName: "DBName", databaseRoot: databaseRoot)
+                                .Options;
+
+            using (var context = new BaseContext(options))
+            {
+                context.Users.Add(new User { Name="Joe"});
+                context.SaveChanges();
+            }
+
+            using (var context = new InheritedContext(options))
+            {
+                context.Users.Add(new User { Name = "Fred" });
+                context.SaveChanges();
+            }
         }
     }
+
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions options) : base(options)
+        {
+                   
+        }
+        public DbSet<User> Users { get; set; }
+    }
+    public class InheritedContext : BaseContext
+    {
+        public InheritedContext(DbContextOptions options) : base(options)
+        {
+             
+        }
+        public DbSet<UserActions> UserActions { get; set; }
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class UserActions
+    {
+        public int ID { get; set; }
+    }
 }
+
+
